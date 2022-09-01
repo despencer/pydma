e8b9fd58ea8369d7954cd109bbb9e550429ce834
despencer <despencer@mail.ru>
2022-09-01 09:46:20 +0300

    Refactoring: Db and DbRun classes created.

--- dbmeta.py
+++ dbmeta.py
@@ -3,30 +3,30 @@ import logging
 from datetime import datetime, timezone
 
 class DbPackaging:
-    def __init__(self):
-        pass
+    def __init__(self, db):
+        self.db = db
 
     def checkregistry(self):
         try:
-            self.cur.execute("SELECT * FROM dbpacket WHERE module = 'init'")
+            self.db.cur.execute("SELECT * FROM dbpacket WHERE module = 'init'")
         except sqlite3.OperationalError:
-            self.cur.execute("CREATE TABLE dbpacket (module TEXT NOT NULL, version INTEGER NOT NULL, deploy INTEGER, PRIMARY KEY (module, version))")
-            self.db.commit()
+            self.db.cur.execute("CREATE TABLE dbpacket (module TEXT NOT NULL, version INTEGER NOT NULL, deploy INTEGER, PRIMARY KEY (module, version))")
+            self.db.db.commit()
             logging.info("Packet registry created")
             self.deploypacket('seqid', 1,"CREATE TABLE seqid_seq (id INTEGER NOT NULL)")
-            self.cur.execute("INSERT INTO seqid_seq (id) VALUES (?)", (1, ) )
-            self.db.commit()
+            self.db.cur.execute("INSERT INTO seqid_seq (id) VALUES (?)", (1, ) )
+            self.db.db.commit()
 
     def ispacketregistered(self, name, version):
-        self.cur.execute("SELECT deploy FROM dbpacket WHERE module = ? AND version = ?", ( name ,version))
-        if self.cur.fetchone() == None:
+        self.db.cur.execute("SELECT deploy FROM dbpacket WHERE module = ? AND version = ?", ( name ,version))
+        if self.db.cur.fetchone() == None:
             return False
         logging.debug('Row fetched from deploy for %s-%s', name, version)
         return True
 
     def registerpacket(self, name, version):
         time = datetime.now(timezone.utc)
-        self.cur.execute("INSERT INTO dbpacket (module, version, deploy) VALUES (?, ?, ?)", (name, version, int(datetime.now(timezone.utc).timestamp())))
+        self.db.cur.execute("INSERT INTO dbpacket (module, version, deploy) VALUES (?, ?, ?)", (name, version, int(datetime.now(timezone.utc).timestamp())))
         logging.info('Packet %s-%s registered', name, version)
 
     def deploypacket(self, name, version, script):
@@ -34,23 +34,54 @@ class DbPackaging:
             try:
                 if isinstance (script, list):
                     for s in script:
-                        self.cur.execute(s)
+                        self.db.cur.execute(s)
                 else:
-                    self.cur.execute(script)
+                    self.db.cur.execute(script)
                 self.registerpacket(name, version)
-                self.db.commit()
+                self.db.db.commit()
                 logging.info('Packet %s version %s deployed', name, version)
             except:
-                self.db.rollback()
+                self.db.db.rollback()
                 raise
 
-    def open(self, name):
-        if name.find('.') < 0:
-                name += '.db'
-        self.db = sqlite3.connect(name)
+class Db:
+    def __init__(self, name):
+        self.filename = name
+        if self.filename.find('.') < 0:
+                self.filename += '.db'
+
+    def open(self):
+        self.db = sqlite3.connect(self.filename)
         self.cur = self.db.cursor()
-        self.checkregistry()
+        DbPackaging(self).checkregistry()
 
     def close(self):
         self.db.close()
+        self.db = None
+        self.cur = None
 
+    def run(self):
+        return DbRun(self)
+
+    def deploypacket(self, name, version, script):
+        DbPackaging(self).deploypacket(name, version, script)
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self, extype, exvalue, extrace):
+        self.close()
+
+class DbRun:
+    def __init__(self, db):
+        self.db = db
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, extype, exvalue, extrace):
+        if extype == None:
+            self.db.commit()
+        else:
+            self.db.rollback()
